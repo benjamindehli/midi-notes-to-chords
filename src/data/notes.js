09bc129432daba40129fc3b1032fb5ddc415b7b9
Benjamin Dehli <superelg@gmail.com>
2026-03-07 17:43:36 +0100

    Add data for notes and chords

---
+++ src/data/notes.js
@@ -0,0 +1,16 @@
+const notes = [
+    { number: 0, name: "C" },
+    { number: 1, name: "C#" },
+    { number: 2, name: "D" },
+    { number: 3, name: "D#" },
+    { number: 4, name: "E" },
+    { number: 5, name: "F" },
+    { number: 6, name: "F#" },
+    { number: 7, name: "G" },
+    { number: 8, name: "G#" },
+    { number: 9, name: "A" },
+    { number: 10, name: "A#" },
+    { number: 11, name: "B" }
+];
+
+export default notes;
