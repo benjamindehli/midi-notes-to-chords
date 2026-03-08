3cee11aadc6dd5af9018b3c684f94cde1c65245e
Benjamin Dehli <superelg@gmail.com>
2026-03-08 01:41:00 +0100

    Add additional exports to index.js
    Expose note helper functions and chord/note data constants to make them available in the public API.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 export { getChordsFromSelectedNotes } from "./helpers/matchHelpers.js";
+export { getRelativeNoteNumber, normalizeHalfStep, normalizeHalfSteps } from "./helpers/noteHelpers.js";
+export { default as chords } from "./data/chords.js";
+export { default as notes } from "./data/notes.js";
