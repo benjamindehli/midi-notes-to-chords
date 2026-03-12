1fe3bc4b1e1cc6d2b19a2d77db17af4792c7e751
Benjamin Dehli <superelg@gmail.com>
2026-03-12 19:31:46 +0100

    Add intervals data
    Provide a collection of predefined musical intervals, mapping common names to their distances in semitones using the Interval class.

---
+++ src/data/intervals.js
@@ -0,0 +1,33 @@
+// Classes
+import Interval from "../classes/Interval";
+
+const intervals = [
+    new Interval({ name: "P1", number: 0 }),
+    new Interval({ name: "m2", number: 1 }),
+    new Interval({ name: "M2", number: 2 }),
+    new Interval({ name: "m3", number: 3 }),
+    new Interval({ name: "M3", number: 4 }),
+    new Interval({ name: "P4", number: 5 }),
+    new Interval({ name: "d5", number: 6 }),
+    new Interval({ name: "P5", number: 7 }),
+    new Interval({ name: "m6", number: 8 }),
+    new Interval({ name: "M6", number: 9 }),
+    new Interval({ name: "m7", number: 10 }),
+    new Interval({ name: "M7", number: 11 }),
+    new Interval({ name: "P8", number: 12 }),
+    new Interval({ name: "m9", number: 13 }),
+    new Interval({ name: "M9", number: 14 }),
+    new Interval({ name: "m10", number: 15 }),
+    new Interval({ name: "M10", number: 16 }),
+    new Interval({ name: "P11", number: 17 }),
+    new Interval({ name: "A11", number: 18 }),
+    new Interval({ name: "P12", number: 19 }),
+    new Interval({ name: "m13", number: 20 }),
+    new Interval({ name: "M13", number: 21 }),
+    new Interval({ name: "m14", number: 22 }),
+    new Interval({ name: "M14", number: 23 }),
+    new Interval({ name: "P15", number: 24 }),
+    new Interval({ name: "A15", number: 25 })
+];
+
+export default intervals;
