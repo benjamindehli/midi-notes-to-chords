e8439c79915bca876ea01f71bbcf6afe99be610c
Benjamin Dehli <superelg@gmail.com>
2026-03-12 20:45:35 +0100

    Export scales data from main entry point
    Include the scales dataset in the public API to provide access to scale definitions alongside existing chord and note data.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { getChordsFromSelectedNotes } from "./helpers/matchHelpers.js";
 export { getRelativeNoteNumber, normalizeHalfStep, normalizeHalfSteps } from "./helpers/noteHelpers.js";
 export { default as chords } from "./data/chords.js";
+export { default as scales } from "./data/scales.js";
 export { default as notes } from "./data/notes.js";
