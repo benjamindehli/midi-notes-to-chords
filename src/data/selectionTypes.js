b6bb2ec80d281085138e9d1d387d44f52191e67b
Benjamin Dehli <superelg@gmail.com>
2026-03-12 19:31:55 +0100

    Add selection types data
    Provide a collection of predefined selection options, mapping display labels for "Chord" and "Scale" to their associated values using the SelectionType class.

---
+++ src/data/selectionTypes.js
@@ -0,0 +1,6 @@
+// Classes
+import SelectionType from "../classes/SelectionType";
+
+const selectionTypes = [new SelectionType({ label: "Chord", value: "chord" }), new SelectionType({ label: "Scale", value: "scale" })];
+
+export default selectionTypes;
