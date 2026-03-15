ee8bdd618d288160bac8ab3f3ef70cf82ea6fd67
Benjamin Dehli <superelg@gmail.com>
2026-03-15 16:43:46 +0100

    Export Interval class and intervals data
    Include the Interval class and the intervals data object in the public API. The export list has also been reordered alphabetically to maintain consistency.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 // Classes
 import Chord from "./classes/Chord";
+import Interval from "./classes/Interval";
 import Midi from "./classes/Midi";
 import NoteSelection from "./classes/NoteSelection";
 import Scale from "./classes/Scale";
 
 // Data
 import chords from "./data/chords";
+import intervals from "./data/intervals";
 import notes from "./data/notes";
 import scales from "./data/scales";
 
-export { chords, scales, notes, Midi, Chord, Scale, NoteSelection };
+export { Chord, Interval, Midi, NoteSelection, Scale, chords, intervals, notes, scales };
 
 export { getChordsFromSelectedNotes } from "./helpers/matchHelpers";
 export { getRelativeNoteNumber, normalizeHalfStep, normalizeHalfSteps } from "./helpers/noteHelpers";
